ca7175567ba34261012eff83216e4de459e3bfba
luisliu <luis.liuli124@gmail.com>
2024-04-07 18:33:35 -0400

    Added a slideshow to spotifywrapped button (Need to make it interactive and adapt to fetched data)

--- app/src/main/java/com/example/spotifywrapped/SpotifyWrappedFragment.java
+++ app/src/main/java/com/example/spotifywrapped/SpotifyWrappedFragment.java
@@ -1,12 +1,15 @@
 package com.example.spotifywrapped;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class SpotifyWrappedFragment extends Fragment {
@@ -18,5 +21,18 @@ public class SpotifyWrappedFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_spotify_wrapped, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        // Find the ImageView by its ID
+        ImageView iv_background = view.findViewById(R.id.iv_background);
+
+        // Assuming iv_background has been assigned an AnimationDrawable as background
+        // either through XML or programmatically before this point
+        if (iv_background.getDrawable() instanceof AnimationDrawable) {
+            AnimationDrawable animationDrawable = (AnimationDrawable) iv_background.getDrawable();
+            animationDrawable.start();
+        }
+    }
 }
